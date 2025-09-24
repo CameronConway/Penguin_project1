e7646e93b61060aa2f36d1b258e6a883d8242f4d
CameronConway <cameronr.conway@btinternet.com>
2025-09-24 14:19:23 +0100

    First commit

---
+++ 1_Penguin.R
@@ -0,0 +1,25 @@
+rm(list=ls()) #reset
+graphics.off()
+#keep project data organised
+#keep separate scripts
+#keep raw read only data by itself
+#keep reports and manuscripts
+#keep figures
+#keep results
+#link figs and results to scripts
+#have readme for description
+
+library(tidyverse)
+penguins <- read.table('data/penguin_data.txt', header = T)
+glimpse(penguins)
+model1 <- lm(body_mass_g ~ flipper_length_mm, data=penguins)
+summary(model1)
+ggplot(penguins, aes(x = flipper_length_mm, y = body_mass_g, colour = 
+  species)) + 
+  geom_point() + 
+  stat_smooth(method = 'lm')
+
+ggsave('figs/1_flipper_bodymass_regression.png')
+
+penguins_female <- subset(penguins, sex == 'female')
+write_tsv(penguins_female, 'results/1_penguin_female_only.txt')
---
+++ README.md
@@ -0,0 +1,2 @@
+raw data in data
+1_penguin started 24/09/25
